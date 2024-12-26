f294ddc6b06ba02454b7685d034a3d525d48ae41
rjrizani <rjrizani@gmail.com>
2024-12-26 16:25:28 +0800

    add condition if sold out

--- hijup/hijup/pipelines.py
+++ hijup/hijup/pipelines.py
@@ -12,6 +12,7 @@ class HijupPipeline:
     def process_item(self, item, spider):
         item['price'] = item['price'][1] if len(item['price'][1]) > 3 else item['price'][0]
         #item['price'] = self.extract_numeric_price(item['price'])
+        item['availability'] = item['availability'].strip() if item['availability'] else "Sold Out"
         return item
     
 
